14c0783b7e6417e63a8822c5ed753acef622d255
Isaac Greenspan <ilg@2718.us>
2010-12-13 16:16:49 -0600

    changing includes to be purely relative

--- XMLRPC.h
+++ XMLRPC.h
@@ -20,8 +20,8 @@
 // IN THE SOFTWARE.
 // 
 
-#import <XMLRPC/XMLRPCConnection.h>
-#import <XMLRPC/XMLRPCConnectionDelegate.h>
-#import <XMLRPC/XMLRPCConnectionManager.h>
-#import <XMLRPC/XMLRPCResponse.h>
-#import <XMLRPC/XMLRPCRequest.h>
+#import "XMLRPCConnection.h"
+#import "XMLRPCConnectionDelegate.h"
+#import "XMLRPCConnectionManager.h"
+#import "XMLRPCResponse.h"
+#import "XMLRPCRequest.h"
